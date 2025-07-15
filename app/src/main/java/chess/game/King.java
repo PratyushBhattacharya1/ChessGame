45187625e3e6946eb4f044953770283eab7333d5
Pratyush-Bhattacharya <prbhattacharya@ucsd.edu>
2025-07-15 21:45:32 +0400

    Finished King Class

--- app/src/main/java/chess/game/King.java
+++ app/src/main/java/chess/game/King.java
@@ -2,6 +2,9 @@ package chess.game;
 
 public class King extends SlidingPieces implements Piece {
 
+    private static final Position[] CASTLE_POSITION_WHITE = {new Position(7, 2), new Position(7, 6)};
+    private static final Position[] CASTLE_POSITION_BLACK = {new Position(0, 2), new Position(0, 6)};
+
     private boolean hasMoved = false;
 
     public King(Position position, Color color) {
@@ -16,24 +19,174 @@ public class King extends SlidingPieces implements Piece {
             newR = targetPosition.getRow(),
             newC = targetPosition.getColumn();
 
-        
-        if (!(Math.abs(r - newR) == 1 || Math.abs(c - newC) == 1) || (r == newR && c == newC) || board[newR][newC] != null && board[newR][newC].getColor() == this.color) 
+        // Check if the player wants to castle
+        if (this.canCastle(targetPosition, board)) return true;
+
+        // Check if new coordinate is possible for king. 
+        if (!(Math.abs(r - newR) == 1 || Math.abs(c - newC) == 1) || (r == newR && c == newC) || board[newR][newC] != null && board[newR][newC].getColor() == this.color)
             return false;
 
+        // Position is viable
         return true;
+    }
+
+    private boolean canCastle(Position targetPosition, Piece[][] board) {
+        // Cannot castle if king moved
+        if (hasMoved) return false;
+
+        // Copy correct potential positions based on color
+        Position[] castlePositions = isWhite() ? CASTLE_POSITION_WHITE : CASTLE_POSITION_BLACK;
+        int row = isWhite() ? 7 : 0;
+
+        // Loop through respective color kingside/queenside castling positions until we hit target position
+        for (int i = 0; i < castlePositions.length; i++) {
+            // Target position matches a castling position. The player wants to castle.
+            if (targetPosition.equals(castlePositions[i])) {
+                // Rook column depends on which castling side we matched with
+                int rookCol = (i == 0) ? 0 : 7;
+                Piece rook = board[row][rookCol];
+                // Check if similarly colored rook piece exists and hasn't moved
+                if (rook != null && rook instanceof Rook && !((Rook)rook).getHasMoved() && rook.getColor() == this.color) {
+                    int kingCol = 4;
+                    int dir = (rookCol == 0) ? -1 : 1;
+                    // Check all squares between king and rook are empty
+                    for (int c = kingCol + dir; c != rookCol; c += dir) {
+                        if (board[row][c] != null) return false;
+                    }
+                    // Check king does not pass through or end up in check
+                    for (int c = kingCol; c != castlePositions[i].getColumn() + dir; c += dir) {
+                        King tempKing = new King(new Position(row, c), this.color);
+                        if (tempKing.isInCheck(board)) return false;
+                    }
+                    // Passed all checks and can castle
+                    return true;
+                }
+            }
+        }
+
+        // Player does not want to castle
+        return false;
     }
 
     public boolean isInCheck(Piece[][] board) {
 
+        // Get current row and column
+        int r = this.position.getRow();
+        int c = this.position.getColumn();
 
+        // Pawn direction to watch out for is fixed for color
+        int pawnDir = isWhite() ? -1 : 1;
+        int nextRow = r + pawnDir;
 
+        // Check left and right columns
+        for (int dc : new int[]{-1, 1}) {
+            int nextCol = c + dc;
+            // Check if the column is out of bounds
+            if (nextCol >= 0 && nextCol < 8) {
+                // Check if piece there is opposite colored pawn
+                Piece piece = board[nextRow][nextCol];
+                if (piece != null && piece instanceof Pawn && piece.getColor() != this.color) {
+                    return true;
+               }
+            }
+        }
 
+        // All 8 possible horse positions
+        int[][] horsePositions = {
+            {r + 2, c + 1},
+            {r + 2, c - 1},
+            {c + 2, r + 1},
+            {c + 2, r - 1},
+            {r - 2, c + 1},
+            {r - 2, c - 1},
+            {c - 2, r + 1},
+            {c - 2, r - 1}
+        };
+
+        // Check every legal horse position for a knight
+        for (int[] hp : horsePositions) {
+
+            int newR = hp[0];
+            int newC = hp[1];
+            // If the position is legal
+            if (newR >= 0 && newR < 8 && newC >= 0 && newC < 8) {
+                // Check for an opposite color knight
+                if (board[newR][newC] != null && board[newR][newC] instanceof Knight && board[newR][newC].getColor() != this.color) return true;
+            }
+        }
+
+        // All 8 directions
+        int[][] directions = {
+            {0, 1},     // right
+            {0, -1},    // left
+            {1, 0},     // down
+            {-1, 0},    // up
+            {-1, 1},    // up-right
+            {-1, -1},   // up-left
+            {1, 1},     // down-right
+            {1, -1}     // down-left
+        };
+
+        // Check each individual line for either queens/rooks/bishops
+        for (int[] dir : directions) {
+            if (this.checkLine(dir[0], dir[1], board)) return true;
+        }
+
+        // Check all around the king for another king
+        for (int[] dir : directions) {
+            int newR = r + dir[0];
+            int newC = c + dir[1];
+
+            // If the position is legal
+            if (newR >= 0 && newR < 8 && newC >= 0 && newC < 8) {
+                // Check opposite color king
+                Piece piece = board[newR][newC];
+                if (piece != null && piece instanceof King && piece.getColor() != this.color) return true;
+            }
+        }
+
+        // Cleared all possible threats 
+        return false;
+    }
+
+    // Helper to check a direction for bishop/rook/queen threats
+    private boolean checkLine(int dr, int dc, Piece[][] board) {
+        // Get current row and column
+        int r = this.position.getRow();
+        int c = this.position.getColumn();
+
+        // Don't accidently check current square
+        r += dr;
+        c += dc;
+
+        // If the position is legal
+        while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+            Piece piece = board[r][c];
+            if (piece != null) {
+                if (piece.getColor() != this.color) {
+                    // Check if the piece is a queen
+                    if (piece instanceof Queen) return true;
+                    // Or check if the piece is rook/bishop depending on the direction being checked
+                    else if ((dr == 0 || dc == 0) && piece instanceof Rook) return true;
+                    else if ((dr != 0 && dc != 0) && piece instanceof Bishop) return true;
+                }
+
+                // If a piece was found but isn't threatening, then skip checking this direction. It means 
+                // the king was blocked by another piece.
+                break;
+            }
+
+            r += dr;
+            c += dc;
+        }
+
+        // No threat in this direction
         return false;
     }
 
     public void move(Position position) {
         super.move(position);
-        hasMoved = true;
+        this.hasMoved = true;
     }
 
 }
--- app/src/main/java/chess/game/SlidingPieces.java
+++ app/src/main/java/chess/game/SlidingPieces.java
@@ -7,65 +7,41 @@ public abstract class SlidingPieces extends PiecesActs {
     }
 
     protected boolean isValidRookMove(Position targetPosition, Piece board[][]) {
-
         int r = this.position.getRow(), 
             c = this.position.getColumn(), 
-            newR = this.position.getRow(), 
-            newC = this.position.getColumn();
+            newR = targetPosition.getRow(), 
+            newC = targetPosition.getColumn();
 
         if ((r != newR && c != newC) || (r == newR && c == newC)) return false;
 
-        if (r == newR) 
-            if (newC > c) {
-                for (int i = c; c < newC; i++) 
-                    if (board[r][i] != null) return false;
-                }
-            else {
-                for (int i = c; c > newC; i--)
-                    if (board[r][i] != null) return false;
-                }
-        else 
-            if (newR > r) {
-                for (int i = r; r < newR; i++)
-                    if (board[i][c] != null) return false;
+        if (r == newR) {
+            int step = (newC > c) ? 1 : -1;
+            for (int i = c + step; i != newC; i += step) {
+                if (board[r][i] != null) return false;
             }
-            else {
-                for (int i = r; r > newR; i--) 
-                    if (board[i][c] != null) return false;
+        } else {
+            int step = (newR > r) ? 1 : -1;
+            for (int i = r + step; i != newR; i += step) {
+                if (board[i][c] != null) return false;
             }
-        
- 
+        }
         return true;
     }
 
     protected boolean isValidBishopMove(Position targetPosition, Piece board[][]) {
-
         int r = this.position.getRow(), 
             c = this.position.getColumn(), 
-            newR = this.position.getRow(), 
-            newC = this.position.getColumn();
+            newR = targetPosition.getRow(), 
+            newC = targetPosition.getColumn();
 
         if (Math.abs(r - newR) != Math.abs(c - newC)) return false;
 
-        if (newR > r && newC > c) {
-            for (int i = 1; i < newR - r; i++) {
-                if (board[r + i][c + i] != null) return false;
-            }
-        } else if (newR > r && newC < c) {
-            for (int i = 1; i < newR - r; i++) {
-                if (board[r + i][c - i] != null) return false;
-            }
-        } else if (newR < r && newC > c) {
-            for (int i = 1; i < r - newR; i++) {
-                if (board[r - i][c + i] != null) return false;
-            }
-        } else if (newR < r && newC < c) {
-            for (int i = 1; i < r - newR; i++) {
-                if (board[r - i][c - i] != null) return false;
+        int rowStep = (newR > r) ? 1 : -1;
+        int colStep = (newC > c) ? 1 : -1;
+        int steps = Math.abs(newR - r);
+        for (int i = 1; i < steps; i++) {
+            if (board[r + i * rowStep][c + i * colStep] != null) return false;
         }
-    }
-
         return true;
-
     }
 }
