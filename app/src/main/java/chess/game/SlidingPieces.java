ce92ba9c7d479730df8f4a4198913da888b471ee
Pratyush-Bhattacharya <prbhattacharya@ucsd.edu>
2025-07-20 06:01:29 +0400

    Fixed loophole with sliding pieces being able to move despite blocked by same color piece.

--- app/src/main/java/chess/game/SlidingPieces.java
+++ app/src/main/java/chess/game/SlidingPieces.java
@@ -58,7 +58,7 @@ public abstract class SlidingPieces extends PieceBehaviors {
         int colDelta = Integer.signum(newC - c);
 
         if (this.processLine(rowDelta, colDelta, board, false, (row, column, piece) -> 
-            piece != null && piece.getColor() == this.color
+            piece != null && !(newR == row && newC == column)
         )) return false;
 
         return true;
@@ -93,7 +93,7 @@ public abstract class SlidingPieces extends PieceBehaviors {
         int colDelta = Integer.signum(newC - c);
 
         if (this.processLine(rowDelta, colDelta, board, false, (row, column, piece) -> 
-            piece != null && newR != row && newC != column && piece.getColor() == this.color
+            piece != null && !(newR == row && newC == column)
         )) return false;
         
         return true;
